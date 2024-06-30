c6a892557a841a612e3f16b9477e8ded97fed51f
Valencastroo <98562751+Valencastroo@users.noreply.github.com>
2024-06-30 06:35:26 -0300

    Add files via upload

--- cbro12.lua
+++ cbro12.lua
@@ -75,14 +75,14 @@ local Window = Library:CreateWindow({
     Discord = {
         Enabled = true,
         Invite = "xM9f3spQAw", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+        RememberJoins = false -- Set this to false to make them join the discord every time they load it up
     },
     KeySystem = true, -- Set this to true to use our key system
     KeySettings = {
         Title = "Sistema de key", -- 
         Subtitle = "Discord",
         Note = "discord.gg/",
-        FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+        FileName = "Llave", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
         SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
         GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
         Key = {"cukytamaluco"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
@@ -94,6 +94,7 @@ local Combat = Window:CreateTab("Combat", 4483362458) -- Title, Image
 local Misc = Window:CreateTab("Misc", 4483362458) -- Title, Image
 local Visuals = Window:CreateTab("Visuals", 4483362458) -- Title, Image
 local World = Window:CreateTab("World", 4483362458) -- Title, Image
+local Creditos = Window:CreateTab("Creditos", 4483362458)
 
 --// Version
 
@@ -123,9 +124,15 @@ if true then
     end
 
     do
+
+        --// Creditos
+
+        Creditos:CreateLabel("Creado por Cuky2008")
+        Creditos:CreateLabel("Aguante el paco")
+
         --// Home
 
-        Home:CreateLabel("Valen Version")
+        Home:CreateLabel("Cuky2008 Version")
         Home:CreateLabel("Version: ".. Version)
     end
 
